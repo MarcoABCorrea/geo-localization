ca63f08729cbaf33bf6c3c28cf7c8353c929d561
Marco Aurélio Batista Corrêa <marcoabc@BH10H07-FSW.acs.com.br>
2015-05-25 11:51:17 -0300

    Implementing tests

--- yeoman/app/scripts/controllers/main.js
+++ yeoman/app/scripts/controllers/main.js
@@ -9,33 +9,32 @@ angular.module('geoLocationApp')
 
     var mapOptions = {
         zoom: 3,
-        // center: new google.maps.LatLng(40.0000, -98.0000),
         center: new google.maps.LatLng(0.0629563,-20.4218893),
         mapTypeId: google.maps.MapTypeId.TERRAIN
     }
     $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
     $scope.markers = [];
-    
+
     var infoWindow = new google.maps.InfoWindow();
-    
+
     var createMarker = function (info){
-        
+
         var marker = new google.maps.Marker({
             map: $scope.map,
             position: new google.maps.LatLng(info.lat, info.lon),
             title: info.org + ' - ' + info.city
         });
         marker.content = '<div class="infoWindowContent">' + info.city + '</div>';
-        
+
         google.maps.event.addListener(marker, 'click', function(){
             infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
             infoWindow.open($scope.map, marker);
         });
-        
+
         $scope.markers.push(marker);
-        
-    }  
+
+    }
 
     $scope.openInfoWindow = function(e, selectedMarker){
         e.preventDefault();
@@ -44,19 +43,17 @@ angular.module('geoLocationApp')
 	//////////////////////// end map functions
 
    $scope.updateLocationDetails = function(data, isMyLocation){
-    $scope.data = data;   
+    $scope.data = data;
 
     if(isMyLocation && !angular.isUndefined(myLocationIndex)){
-    	$scope.markers.splice(myLocationIndex, 1);	
+    	$scope.markers.splice(myLocationIndex, 1);
     }
 
-	createMarker(data);
+  	createMarker(data);
 
-	if(isMyLocation){
-		myLocationIndex = $scope.markers.length -1;
-	}
-
-
+  	if(isMyLocation){
+  		myLocationIndex = $scope.markers.length -1;
+  	}
 
     var now = new Date();
 
@@ -86,9 +83,9 @@ angular.module('geoLocationApp')
   $scope.resetLocationDetails = function() {
     if(!angular.isUndefined(myLocationIndex)){
     	$scope.markers[myLocationIndex].setMap(null);
-    	$scope.markers.splice(myLocationIndex, 1);	
+    	$scope.markers.splice(myLocationIndex, 1);
 	    myLocationIndex = undefined;
-    }	
+    }
     $scope.data = {query: "0.0.0.0"};
     $("table").addClass("empty");
   }
--- yeoman/tests/geo-location_tests.js
+++ yeoman/tests/geo-location_tests.js
@@ -2,27 +2,99 @@
 'use strict';
 describe('Geo Location page', function() {
 
-  beforeEach(function () {
-    browser.get('http://127.0.0.1:9000/#/');
-  });
-
   // Components
   var btnSiteLocation = element(by.id('btnSiteLocation')),
     btnResetLocation = element(by.id('btnResetLocation')),
     btnMyLocation = element(by.id('btnMyLocation')),
     iptSite = element(by.model('site')),
     tblResult = element(by.tagName('table')),
-    map = element(by.id('map'))
-    ;
+    map = element(by.id('map')),
+    tblRow = element.all(by.css('.field_name')),
+    tblRowLocation = element.all(by.css('.location_value')),
+    markers = element.all(by.repeater('marker in markers'));
+
+  // Tests Variables
+  var rowNames = ["IP", "Country", "Region", "City", "Time Zone", "Latitude", "Longitude"],
+    tblRowLength = 0,
+    tblRowLocationLength = 0;
+
+  beforeEach(function () {
+    browser.get('http://127.0.0.1:9000/#/');
+    tblRow.count().then(function(count){
+      tblRowLength = count;
+    });
+    tblRowLocation.count().then(function(count){
+      tblRowLocationLength = count;
+    });
+  });
 
   it('should have 3 buttons, 1 input, 1 table, 1 map', function () {
-
     expect(btnSiteLocation.isPresent()).toBe(true);
     expect(btnResetLocation.isPresent()).toBe(true);
     expect(btnMyLocation.isPresent()).toBe(true);
     expect(iptSite.isPresent()).toBe(true);
     expect(tblResult.isPresent()).toBe(true);
     expect(map.isPresent()).toBe(true);
+  });
+
+  it('should have 3 buttons with names "Locate", "My Location", "Reset Location"', function () {
+    expect(btnSiteLocation.getText()).toEqual('Locate');
+    expect(btnMyLocation.getText()).toEqual('My location');
+    expect(btnResetLocation.getText()).toEqual('Reset location');
+  });
+
+  it('should have 1 table with rows names "IP", "Country", "Region", "City", "Time Zone", "Latitude", "Longitude"', function () {
+    expect(tblRow.count()).toBe(7);
+    for (var i = tblRowLength -1; i >= 0; i--) {
+      expect(tblRow.get(i).getText()).toEqual(rowNames[i]);
+    };
+  });
+
+  it('should show your location when you click the "My Location" button', function () {
+    btnMyLocation.click();
+    setTimeout(function() {
+      expect(tblRowLocation.count()).toBe(7);
+      for (var i = tblRowLocationLength - 1; i >= 0; i--) {
+        expect(tblRowLocation.get(i).getText().isPresent()).toBe(true);
+      };
+    }, 10);
+  });
+
+  it('should add a mark on map when you click the "My Location" button', function () {
+    btnMyLocation.click().then(function(){
+      expect(markers.count()).toBe(1);
+    });
+  });
+
+  it('should remove the "My Location" mark when click the "Reset location" button', function () {
+    btnMyLocation.click().then(function(){
+      expect(markers.count()).toBe(1);
+      btnResetLocation.click();
+      expect(markers.count()).toBe(0);
+    });
+  });
+
+  it('should add a mark on map when you insert an URL and click the "Locate" button', function () {
+    iptSite.sendKeys('www.google.com');
+    btnSiteLocation.click().then(function(){
+      expect(markers.count()).toBe(1);
+    });
+  });
+
+  it('should remove only the "My Location" mark when added my location and a site location', function () {
+    iptSite.sendKeys('www.google.com');
+    btnSiteLocation.click().then(function(){
+      expect(markers.count()).toBe(1);
+    });
+    iptSite.sendKeys('avenuecode.com');
+    btnSiteLocation.click().then(function(){
+      expect(markers.count()).toBe(2);
+    });
+    btnMyLocation.click().then(function(){
+      expect(markers.count()).toBe(3);
+      btnResetLocation.click();
+      expect(markers.count()).toBe(2);
+    });
   });
 
 });
